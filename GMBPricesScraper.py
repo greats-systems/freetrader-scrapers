a97fe9740363b204fbbdec9e3a001002270b1d32
unknown <48543590+Ngqabutho-Moyo@users.noreply.github.com>
2025-02-27 16:14:36 +0200

    Corrected the API keys

--- GMBPricesScraper.py
+++ GMBPricesScraper.py
@@ -22,12 +22,17 @@ cols[1]=''.join(cols[1].split())
 df.rename(columns={0: cols[0], 1: cols[1]}, inplace=True)
 df=df.iloc[1:]
 df.reset_index(drop=True, inplace=True)
-df.replace(np.nan, '', inplace=True)
+df.replace(np.nan, None, inplace=True)
+df['ProducerPrice']=df['ProducerPrice'].map(
+    lambda x: str(x)[1:]
+    if str(x)[0]=='$'
+    else x
+)
 
-supabase: Client = create_client(URL, ANON_KEY)
+supabase: Client = create_client(URL, SERVICE_KEY)
 
 for row in df.iterrows():
-    supabase.table("Commodities").insert({"CommodityName":row[1][0], "CommodityPrice":row[1][1]}).execute()
+    supabase.table("Commodities").insert({"CommodityName":row[1][0], "CommodityProducePrice":row[1][1]}).execute()
 
 
 
